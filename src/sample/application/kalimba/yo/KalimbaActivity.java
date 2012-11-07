7849c371029cb90bc79e3366f0059fcc15d4a0c2
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2012-11-07 16:28:39 +0900

    11/7 16:28 完成

---
+++ src/sample/application/kalimba/yo/KalimbaActivity.java
@@ -0,0 +1,51 @@
+package sample.application.kalimba.yo;
+
+import android.app.Activity;
+import android.content.res.TypedArray;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class KalimbaActivity extends Activity implements OnClickListener {
+
+	MediaPlayer[] mp;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		TypedArray notes = getResources().obtainTypedArray(R.array.notes);
+		TypedArray buttons = getResources().obtainTypedArray(R.array.buttons);
+		mp = new MediaPlayer[notes.length()];
+
+		for (int i = 0; i < notes.length(); i++) {
+			mp[i] = MediaPlayer.create(this, notes.getResourceId(i, -1));
+		}
+
+		for (int i = 0; i < buttons.length(); i++) {
+			findViewById(buttons.getResourceId(i, -1)).setOnClickListener(this);
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+
+		TypedArray buttons = getResources().obtainTypedArray(R.array.buttons);
+
+		for (int i = 0; i < buttons.length(); i++) {
+			mp[i].seekTo(0);
+			mp[i].start();
+			break;
+		}
+	}
+}
